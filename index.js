6e989b6f56e295300d03f61bf266dae0582fdd84
swaren700-lab <swaren700@gmail.com>
2026-03-13 21:28:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
 const http = require('http');
 const net = require('net');
+const tls = require('tls');
+
 const PORT = process.env.PORT || 8080;
-const SSH_HOST = process.env.SSH_HOST || '127.0.0.1';
-const SSH_PORT = parseInt(process.env.SSH_PORT) || 80;
+const TARGET_HOST = process.env.TARGET_HOST || 'fr-full.privateip.net';
+const TARGET_PORT = parseInt(process.env.TARGET_PORT) || 443;
+
 const server = http.createServer((req, res) => {
-res.writeHead(200, {'Content-Type': 'text/plain'});
-res.end('OK');
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('OK');
 });
+
 server.on('upgrade', (req, socket, head) => {
-const conn = net.connect(SSH_PORT, SSH_HOST, () => {
-socket.write('HTTP/1.1 101 Switching Protocols\r\n\r\n');
-conn.pipe(socket);
-socket.pipe(conn);
+  const conn = tls.connect({
+    host: TARGET_HOST,
+    port: TARGET_PORT,
+    servername: TARGET_HOST,
+    rejectUnauthorized: false
+  }, () => {
+    const wsKey = req.headers['sec-websocket-key'];
+    const path = req.url || '/ws';
+    conn.write(
+      `GET ${path} HTTP/1.1\r\n` +
+      `Host: ${TARGET_HOST}\r\n` +
+      `Upgrade: websocket\r\n` +
+      `Connection: Upgrade\r\n` +
+      `Sec-WebSocket-Key: ${wsKey}\r\n` +
+      `Sec-WebSocket-Version: 13\r\n\r\n`
+    );
+    socket.write('HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\n\r\n');
+    conn.pipe(socket);
+    socket.pipe(conn);
+  });
+  conn.on('error', () => socket.destroy());
+  socket.on('error', () => conn.destroy());
 });
-conn.on('error', () => socket.destroy());
-socket.on('error', () => conn.destroy());
-});
+
 server.listen(PORT, '0.0.0.0', () => {
-console.log('Server running on port ' + PORT);
+  console.log('Server running on port ' + PORT);
 });
