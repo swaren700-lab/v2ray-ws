79cf456c92dafd0ec91085a35f7001023fc348f1
swaren700-lab <swaren700@gmail.com>
2026-03-13 19:58:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 const http = require('http');
 const net = require('net');
-const { createServer } = require('http');
 
 const PORT = process.env.PORT || 8080;
 const SSH_HOST = process.env.SSH_HOST || '127.0.0.1';
-const SSH_PORT = process.env.SSH_PORT || 22;
+const SSH_PORT = process.env.SSH_PORT || 80;
 
-createServer((req, res) => {
+const server = http.createServer((req, res) => {
   res.writeHead(200);
   res.end('ok');
-}).on('upgrade', (req, socket, head) => {
+});
+
+server.on('upgrade', (req, socket, head) => {
   const conn = net.connect(SSH_PORT, SSH_HOST, () => {
     socket.write('HTTP/1.1 101 Switching Protocols\r\n\r\n');
     conn.pipe(socket);
     socket.pipe(conn);
   });
   conn.on('error', () => socket.destroy());
-}).listen(PORT, () => {
+  socket.on('error', () => conn.destroy());
+});
+
+server.listen(PORT, () => {
   console.log('Server running on port ' + PORT);
 });
